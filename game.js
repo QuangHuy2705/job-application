226c3c5ba705fa1816d2173318a73083fe970c0e
Huy Nguyen Thac <ohung004@edu.xamk.fi>
2018-08-13 12:57:16 +0300

    ready

---
+++ game.js
@@ -0,0 +1,19 @@
+
+function gamePlay() {
+	for(let i = 0; i < 101; i++) {
+		if(i % 15 = 0) {
+			console.log("Sphinx Software")
+		}
+		else if(i % 3 == 0) {
+			console.log("Sphinx");
+		}
+		else if (i % 5 == 0) {
+			console.log("Software");
+		} else {
+			console.log(i);
+		}
+	}
+	
+};
+
+gamePlay();
